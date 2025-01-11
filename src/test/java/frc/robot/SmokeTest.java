60f20c8d53d636b9b6c3a45a4e315fb6c2c653a5
Monaghan, Sean P <sean.monaghan@jhuapl.edu>
2025-01-11 15:21:07 -0500

    Initial github CI pipeline

---
+++ src/test/java/frc/robot/SmokeTest.java
@@ -0,0 +1,26 @@
+package frc.robot;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/** A smoke test to ensure JUnit and the test stage of our GitHub actions execute as expected */
+public class SmokeTest {
+
+  @BeforeEach
+  public void setup() {
+    // noop
+  }
+
+  @AfterEach
+  public void tearDown() {
+    // noop
+  }
+
+  @Test
+  public void testSmoke() {
+    assertTrue(true);
+  }
+}
